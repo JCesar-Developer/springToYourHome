9f0c9a4da8ad3364dfc7448fb9930fb6307c27c4
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2022-05-03 15:57:08 +0200

    Spring 2.5.6 with SpringFox 2.9.2

--- src/main/java/com/losAmos/demoLosAmos/config/SecurityConfiguration.java
+++ src/main/java/com/losAmos/demoLosAmos/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,14 +44,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 
         //PERMISSION SETTINGS
             .antMatchers("/", "/login/**", "/register/**", "/js/**", "/css/**", "/images/**").permitAll()
-            .antMatchers().permitAll()
-            .antMatchers("/admin/**", "/api/dishes/**", "/api/admin/dishes/**").hasAnyRole("ADMIN")
-            .antMatchers("/user/**", "/api/user/dishes/**").hasAnyRole("USER")
+            .antMatchers("/open/**", "/api/**/open/**").permitAll()
+            .antMatchers("/user/**", "/api/**/user/**").hasAnyRole("USER")
+            .antMatchers("/admin/**", "/api/**/admin/**").hasAnyRole("ADMIN")
             .anyRequest().authenticated()
 
         //UNCOMMENT THESE LINES WHEN YOU WANTED TO WORK WITH THE JPA_API
--- src/main/java/com/losAmos/demoLosAmos/controllers/DishRestController.java
+++ src/main/java/com/losAmos/demoLosAmos/controllers/DishRestController.java
@@ -1,26 +1,52 @@
 package com.losAmos.demoLosAmos.controllers;
 
 import com.losAmos.demoLosAmos.models.entity.Dish;
-import com.losAmos.demoLosAmos.models.services.GenericServiceAPI;
+import com.losAmos.demoLosAmos.models.impl.DishServiceImpl;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/dishes")
+@RequestMapping("/api")
+@Api(value="", tags="DISHES API REST")
 public class DishRestController {
 
     @Autowired
-    @Qualifier("dishServiceImpl")
-    private GenericServiceAPI dishAPI;
+    private DishServiceImpl dishAPI;
 
-    @GetMapping(value = "/getAll")
-    public List<Dish> getAllDishesREST() {
+    // ------------------- CREATE & UPDATE -------------------- //
+    @PostMapping(value = "/admin/save")
+    public ResponseEntity<Dish> dishesRESTSave(@RequestBody Dish dish) {
+        Dish newDish = dishAPI.save(dish);
+        return new ResponseEntity<Dish>(newDish, HttpStatus.CREATED) ;
+    }
+
+    // ------------------------- READ ------------------------- //
+    @GetMapping(value = "/open/getOne/{id}")
+    public Dish dishesRESTGetOne(@PathVariable Long id) {
+        return dishAPI.getOne(id);
+    }
+
+    @GetMapping(value = "/open/getAll")
+    public List<Dish> dishesRESTGetAll() {
         return dishAPI.getAll();
+    }
+    // ------------------------ DELETE ------------------------ //
+    @PostMapping(value = "/admin/delete/{id}")
+    public ResponseEntity<Dish> dishesRESTDelete(@PathVariable Long id) {
+        Dish dish = dishAPI.getOne(id);
+
+        if( dish != null ) {
+            dishAPI.delete(id);
+        } else {
+            return new ResponseEntity<Dish>(dish, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Dish>(dish, HttpStatus.OK);
     }
 
 }
